c519a838fe0c2788051c8e96f6bbb27ccc5d281b
MartinLaptop <martinbachmor@gmail.com>
2022-08-31 13:44:50 +0200

    Mixin removals+mods.toml update

--- src/main/java/smartin/tetraticcombat/mixin/WeaponRegistryMixin.java
+++ src/main/java/smartin/tetraticcombat/mixin/WeaponRegistryMixin.java
@@ -1,19 +1,13 @@
 package smartin.tetraticcombat.mixin;
 
-import net.bettercombat.api.AttributesContainer;
 import net.bettercombat.api.WeaponAttributes;
 import net.bettercombat.logic.WeaponRegistry;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import smartin.tetraticcombat.Config;
-
-import java.util.Map;
 
 
 @Mixin(value = WeaponRegistry.class, remap = false)
@@ -21,26 +15,19 @@ public abstract class WeaponRegistryMixin {
 
     @Inject(
             at = @At(value = "HEAD"),
-            method = "loadAttributes",
-            cancellable = true,
-            remap = false
-    )
-    private static void loadAttributes(ResourceManager resourceManager, CallbackInfo ci){
-        ci.cancel();
-        WeaponRegistryInterface.loadContainers(resourceManager);
-        Map<ResourceLocation, AttributesContainer> containers = WeaponRegistryInterface.containers();
-        containers.forEach((itemId, container) -> {
-            WeaponRegistry.resolveAndRegisterAttributes(itemId, container);
-        });
-    }
-
-    @Inject(
-            at = @At(value = "HEAD"),
             method = "Lnet/bettercombat/logic/WeaponRegistry;getAttributes(Lnet/minecraft/world/item/ItemStack;)Lnet/bettercombat/api/WeaponAttributes;",
             cancellable = true,
-            remap = false
+            remap = false,
+            require = 1
     )
     private static void getAttributes(ItemStack itemStack, CallbackInfoReturnable<WeaponAttributes> cir){
+        /**
+         * This Mixin is by no means required.
+         * Its only here to allow old items without the Tag to be loaded Correctly
+         * It checks if an Item does not have the Tag set and if its supposed to have a Tag
+         * If both are true it generates a new Tag
+         * There is no harm if this mixins fails, nor will the game crash
+         */
         if(itemStack!=null && itemStack.hasTag() && !itemStack.getTag().contains("weapon_attributes")){
             Config.generateBetterCombatNBT(itemStack);
         }
