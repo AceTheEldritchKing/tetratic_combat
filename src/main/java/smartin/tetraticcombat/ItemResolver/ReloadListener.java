69a9fac46f279e2e710e77d1534ca5967c438c49
MartinLaptop <martinbachmor@gmail.com>
2023-04-08 01:00:25 +0200

    i guess i forgot to push? idk

--- src/main/java/smartin/tetraticcombat/ItemResolver/ReloadListener.java
+++ src/main/java/smartin/tetraticcombat/ItemResolver/ReloadListener.java
@@ -2,7 +2,7 @@ package smartin.tetraticcombat.ItemResolver;
 
 import com.google.gson.Gson;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.packs.resources.Resource;
+import net.minecraft.server.packs.PackType;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.server.packs.resources.SimplePreparableReloadListener;
 import net.minecraft.util.profiling.ProfilerFiller;
@@ -10,36 +10,58 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.BufferedReader;
-import java.io.IOException;
-import java.util.Map;
+import java.util.Iterator;
 
 public class ReloadListener extends SimplePreparableReloadListener {
     private static final Logger LOGGER = LogManager.getLogger();
     @Override
     protected Object prepare(ResourceManager resourceManager, ProfilerFiller profilerFiller) {
         String dataFolder = "configs";
-        Map<ResourceLocation,Resource> files = resourceManager.listResources(dataFolder, (fileName) -> fileName.toString().endsWith(".json"));
+        Iterator<ResourceLocation> iterator = resourceManager.listResources(dataFolder, (fileName) -> fileName.toString().endsWith(".json")).keySet().iterator();
         JSONFormat mergedConfig = new JSONFormat();
         mergedConfig.Version = 1.0d;
 
-        files.forEach((resourceLocation, resource) -> {
-            if(resourceLocation.getNamespace().equals("tetratic")){
-                try {
-                    LOGGER.info("Loading "+resource.toString());
-                    BufferedReader reader = resource.openAsReader();
-                    try{
-                        JSONFormat currentFile = new Gson().fromJson(reader, JSONFormat.class);
-                        mergedConfig.merge(currentFile);
-                    } catch (Exception jsonParsing){
-                        LOGGER.warn("Exception loading tetratic Configuration from Datapack");
-                        LOGGER.warn(jsonParsing.getMessage());
-                        LOGGER.warn(resourceLocation);
-                    }
-                } catch (IOException ignored) {
-                    LOGGER.info("Tetratic failure loading file "+resourceLocation.toString());
+        resourceManager.listPacks().forEach(packResources -> {
+            Iterator<ResourceLocation> iterator2 = resourceManager.listResources(dataFolder, (fileName) -> fileName.toString().endsWith(".json")).keySet().iterator();
+            while(iterator2.hasNext()) {
+                ResourceLocation identifier = iterator2.next();
+                if(identifier.getNamespace().equals("tetratic")){
+                    LOGGER.warn("Tetratic "+identifier.toString());
+                    resourceManager.getResource(identifier).ifPresent(resource -> {
+                        try {
+                            BufferedReader reader = resource.openAsReader();
+                            JSONFormat currentFile = new Gson().fromJson(reader, JSONFormat.class);
+                            mergedConfig.merge(currentFile);
+                        } catch (Exception JsonParsing) {
+                            LOGGER.warn("Exception loading tetratic Configuration from Datapack");
+                            LOGGER.warn(JsonParsing.getMessage());
+                            LOGGER.warn(identifier.toString());
+                        }
+                    });
                 }
             }
         });
+
+        /*
+        while(iterator.hasNext()) {
+            ResourceLocation identifier = iterator.next();
+            if(identifier.getNamespace().equals("tetratic")){
+                LOGGER.warn("Tetratic "+identifier.toString());
+                resourceManager.getResource(identifier).ifPresent(resource -> {
+                    try {
+                        BufferedReader reader = resource.openAsReader();
+                        JSONFormat currentFile = new Gson().fromJson(reader, JSONFormat.class);
+                        mergedConfig.merge(currentFile);
+                    } catch (Exception JsonParsing) {
+                        LOGGER.warn("Exception loading tetratic Configuration from Datapack");
+                        LOGGER.warn(JsonParsing.getMessage());
+                        LOGGER.warn(identifier.toString());
+                    }
+                });
+            }
+        }
+
+         */
         LOGGER.info(mergedConfig.toString());
         return mergedConfig;
     }
