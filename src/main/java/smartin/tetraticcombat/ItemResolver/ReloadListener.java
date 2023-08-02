2f9cd637012c2327a4f39347a2d93fa7260c601f
Smartin <martinbachmor@gmail.com>
2023-08-03 00:40:10 +0200

    -fix load logic - this time for real

--- src/main/java/smartin/tetraticcombat/ItemResolver/ReloadListener.java
+++ src/main/java/smartin/tetraticcombat/ItemResolver/ReloadListener.java
@@ -10,10 +10,13 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.BufferedReader;
+import java.util.Collection;
 import java.util.Iterator;
+import java.util.List;
 
 public class ReloadListener extends SimplePreparableReloadListener {
     private static final Logger LOGGER = LogManager.getLogger();
+
     @Override
     protected Object prepare(ResourceManager resourceManager, ProfilerFiller profilerFiller) {
         String dataFolder = "configs";
@@ -22,11 +25,14 @@ public class ReloadListener extends SimplePreparableReloadListener {
         mergedConfig.Version = 1.0d;
 
         resourceManager.listPacks().forEach(packResources -> {
-            Iterator<ResourceLocation> iterator2 = resourceManager.listResources(dataFolder, (fileName) -> fileName.toString().endsWith(".json")).keySet().iterator();
-            while(iterator2.hasNext()) {
-                ResourceLocation identifier = iterator2.next();
-                if(identifier.getNamespace().equals("tetratic")){
-                    LOGGER.warn("Tetratic "+identifier.toString());
+            //packResources.getResources()
+
+            Collection<ResourceLocation> packResourcesResources = packResources.getResources(PackType.SERVER_DATA, "tetratic", "", (fileName) -> fileName.toString().endsWith(".json"));
+            if (packResourcesResources.size() > 0) {
+                LOGGER.info("Tetratic Loading Config from " + packResources.getName());
+            }
+            packResourcesResources.forEach(identifier -> {
+                if (identifier.getNamespace().equals("tetratic")) {
                     resourceManager.getResource(identifier).ifPresent(resource -> {
                         try {
                             BufferedReader reader = resource.openAsReader();
@@ -39,35 +45,14 @@ public class ReloadListener extends SimplePreparableReloadListener {
                         }
                     });
                 }
-            }
+            });
         });
-
-        /*
-        while(iterator.hasNext()) {
-            ResourceLocation identifier = iterator.next();
-            if(identifier.getNamespace().equals("tetratic")){
-                LOGGER.warn("Tetratic "+identifier.toString());
-                resourceManager.getResource(identifier).ifPresent(resource -> {
-                    try {
-                        BufferedReader reader = resource.openAsReader();
-                        JSONFormat currentFile = new Gson().fromJson(reader, JSONFormat.class);
-                        mergedConfig.merge(currentFile);
-                    } catch (Exception JsonParsing) {
-                        LOGGER.warn("Exception loading tetratic Configuration from Datapack");
-                        LOGGER.warn(JsonParsing.getMessage());
-                        LOGGER.warn(identifier.toString());
-                    }
-                });
-            }
-        }
-
-         */
         LOGGER.info(mergedConfig.toString());
         return mergedConfig;
     }
 
     @Override
     protected void apply(Object mergedConfig, ResourceManager resourceManager, ProfilerFiller profilerFiller) {
-        Resolver.reload((JSONFormat)mergedConfig);
+        Resolver.reload((JSONFormat) mergedConfig);
     }
 }
